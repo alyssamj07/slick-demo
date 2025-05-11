d9105a36bb6512800bad5c24af6f0f890bd656f4
Alyssa J <amjoseph1@ualr.edu>
2025-05-11 15:03:36 +0000

    Complete slick demo with centered slider and pause/play buttons

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,11 +1,21 @@
 $(document).ready(function(){
-    $('.slider').slick({
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000
-    });
+  const $slider = $('.slider');
+
+  $slider.slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
+
+  $('#pause').on('click', function(){
+    $slider.slick('slickPause');
+  });
+
+  $('#play').on('click', function(){
+    $slider.slick('slickPlay');
+  });
 });
+
